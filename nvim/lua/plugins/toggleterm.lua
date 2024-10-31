3de68a1087d0c8caa0f21a5267a2dd1152cf8d7b
willks <william.k.sefton@gmail.com>
2024-10-31 14:30:51 -0400

    updating toggle term keymaps

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -5,17 +5,16 @@ return {
 		opts = {},
 		config = function()
 			require("toggleterm").setup({
-				open_mapping = [[<c-\>]], -- Toggle terminal with Ctrl + \
-				direction = "horizontal", -- Options: 'horizontal', 'vertical', 'tab', 'float'
+				open_mapping = [[<c-\>]],
+				direction = "horizontal",
 			})
-
-			-- Additional keybindings for convenience
 			local keymap = vim.api.nvim_set_keymap
-			keymap("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true }) -- Open/close terminal with <leader>t
-			keymap("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true }) -- Exit terminal mode with Esc
-			keymap("n", "<leader>1", ":ToggleTerm 1<CR>", { noremap = true, silent = true })
-			keymap("n", "<leader>2", ":ToggleTerm 2<CR>", { noremap = true, silent = true })
-			keymap("n", "<leader>3", ":ToggleTerm 3<CR>", { noremap = true, silent = true })
+            local opts = { noremap = true, silent = true }
+			keymap("n", "<leader>t", ":ToggleTerm<CR>", opts)
+			keymap("t", "<Esc>", [[<C-\><C-n>]], opts)
+			keymap("n", "<leader>1", ":ToggleTerm 1<CR>", opts)
+			keymap("n", "<leader>2", ":ToggleTerm 2<CR>", opts)
+			keymap("n", "<leader>3", ":ToggleTerm 3<CR>", opts)
 		end,
 	},
 }
